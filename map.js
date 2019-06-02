a0cc7270bbc73470bb820814f7961d8a5c2825da
rishinath4159 <rishi.nath@gmail.com>
2019-06-02 08:23:59 -0700

    temp. solution to show territory names

--- map.js
+++ map.js
@@ -1,3 +1,5 @@
+function run() {
+
 let guildTerritories = [];
 let images = [];
 let rectangles = [];
@@ -9,65 +11,67 @@ let guildPrefs = {
 	}
 }
 
-const visited = localStorage.getItem('visited');
-if (!visited) {
+try {
+	const visited = localStorage.getItem("visited");
+	if (!visited) {
 	    alert("If you would like to add your guild's preferences (color and prefix), please contact brokenmotor in-game or through discord at regale#5688. Enjoy!");
-	    localStorage.setItem('visited', true);
+	    localStorage.setItem("visited", true);
 	}
-
-function init() {
-	const map = L.map('map', {
-    	crs: L.CRS.Simple
-	});
-
-	map.fitBounds([[0, -4], [6, 2]]);
-
-	for (let a = 0; a < 3; a++) {
-		for (let b = 0; b < 3; b++) {
-			bounds.push([[a*2,(2*b)-4], [(a+1)*2, (2*(b+1))-4]])
-		}	
-	}
-
-	for (let bound of bounds) {
-		images.push(L.imageOverlay(`./tiles/${bound[0][1]}/${bound[0][0]}.png`, bound));
-	}
-
-	for (let image of images) {
-		image.addTo(map);
-	}
+} catch (e) {
+	console.error(e);
+}	
 
 
-	fetch('https://raw.githubusercontent.com/DevScyu/Wynn/master/territories.json')
+const map = L.map("map", {    	
+	crs: L.CRS.Simple
+});	
+
+map.fitBounds([[0, -4], [6, 2]]);
+
+for (let a = 0; a < 3; a++) {
+	for (let b = 0; b < 3; b++) {
+		bounds.push([[a*2,(2*b)-4], [(a+1)*2, (2*(b+1))-4]])
+	}	
+}
+
+for (let bound of bounds) {
+	images.push(L.imageOverlay(`./tiles/${bound[0][1]}/${bound[0][0]}.png`, bound));
+}
+
+for (let image of images) {
+	image.addTo(map);
+}
+
+
+fetch("https://raw.githubusercontent.com/DevScyu/Wynn/master/territories.json")
 	    .then(response =>
-	    	response.json())
+	    response.json())
 	    .then(json => {
-		    for (let territory of json) {
-		    	let bounds = [territory["start"].split(','), territory["end"].split(',')];
+		   for (let territory of json) {
+		    let bounds = [territory["start"].split(","), territory["end"].split(",")];
 
-		    	for (let i in bounds) {	
-		    		bounds[i][0] *= .001
-		    		bounds[i][1] *= .001
+		    for (let i in bounds) {	
+		    	bounds[i][0] *= .001
+		   		bounds[i][1] *= .001
 
-		    	}
+		   	}
 
-		    	bounds[0].reverse();
-		    	bounds[1].reverse();
+	    	bounds[0].reverse();
+	    	bounds[1].reverse();
 
-
-		    	bounds[0][0] *= -1;
-		    	bounds[1][0] *= -1;
-		    	let rectangle = L.rectangle(bounds, 
+	    	bounds[0][0] *= -1;
+	    	bounds[1][0] *= -1;
+	    	let rectangle = L.rectangle(bounds, 
 				{color: "#ff7800", weight: 1}).bindTooltip("",
-		   		{className: "guild-name", permanent: true, direction:"center"}
-		  		).openTooltip()
+		   		{className: "guild-name", permanent: true, direction:"center"}).openTooltip();
 
-		    	rectangles[territory["name"]] = rectangle;
-		  		rectangle.addTo(map);
-	    		}	
+	    	rectangles[territory["name"]] = rectangle;
+	  		rectangle.addTo(map);
+	    	}	
 		}).then(_ => {
 		  	update();
 		});
-}
+
 
 
 function update() {
@@ -77,32 +81,35 @@ function update() {
 	.then(guildTerritories => {
 		Object.keys(guildTerritories).forEach(territory => {
 
-		let guild = guildTerritories[territory]["guild"]
+			let guild = guildTerritories[territory]["guild"];
 
-		if (!(Object.keys(guildPrefs).includes(guild))) {
-			guildPrefs[guild] = {};
-			guildPrefs[guild]["color"] = hex();
+			if (!(Object.keys(guildPrefs).includes(guild))) {
+				guildPrefs[guild] = {};
+				guildPrefs[guild]["color"] = hex();
 
-			fetch(`https://api.wynncraft.com/public_api.php?action=guildStats&command=${guild}`)
-				.then(response => response.json())
-				.then(json => {guildPrefs[guild]["prefix"] = json["prefix"]})
-				.then(_ => {
-					rectangles[territory].setStyle({
-						color: guildPrefs[guild]["color"],
+				fetch(`https://api.wynncraft.com/public_api.php?action=guildStats&command=${guild}`)
+					.then(response => response.json())
+					.then(json => {guildPrefs[guild]["prefix"] = json["prefix"]})
+					.then(_ => {
+						rectangles[territory].setStyle({
+							color: guildPrefs[guild]["color"],
+						})
 					})
-					rectangles[territory].setTooltipContent(guildPrefs[guild]["prefix"]);
-				})
-		}	else {
+			} else {
 				rectangles[territory].setStyle({
 					color: guildPrefs[guild]["color"],
 				})
-				rectangles[territory].setTooltipContent(guildPrefs[guild]["prefix"]);
 			}	
+			if (map.getZoom() > 8) {
+				rectangles[territory].setTooltipContent(guildPrefs[guild]["prefix"] + "<br>" + territory);
+			} else {
+				rectangles[territory].setTooltipContent(" ");
+			}
 		
 	});
 
 	setTimeout(_ => 
-		{ console.log("Updating..."); update(); }, 3000);
+		{ console.log("Updating..."); update(); }, 1000);
 	})
 }
 
@@ -110,5 +117,7 @@ function hex() {
 	return "#000000"
 	.replace(/0/g, _ => 
 		(~~(Math.random()*16)).toString(16));
+}
+
 
 }
